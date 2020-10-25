debe7414a0a84d2682029e147177207cfd6fe353
rAmir-ByteGrowth <ranaamir347@gmail.com>
2020-10-25 11:13:28 +0500

    workmanager fine

--- app/src/main/java/com/example/assessmenttest/MainActivity.kt
+++ app/src/main/java/com/example/assessmenttest/MainActivity.kt
@@ -9,6 +9,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : FragmentActivity() {
 
+    var tabPosition:Int=0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,11 +20,23 @@ class MainActivity : FragmentActivity() {
             TabLayoutMediator.TabConfigurationStrategy { tab, position ->
                 if (position ==0){
                     tab.text = "Posts "
+                    tabPosition=0
                 }else{
                     tab.text = "Fav Posts "
+                    tabPosition=1
                 }
 
             }).attach()
+
+    }
+
+    override fun onBackPressed() {
+
+        if (viewPager.currentItem ==1){
+            viewPager.setCurrentItem(0)
+        }else{
+            super.onBackPressed()
+        }
     }
 
     private fun fragmentsAdapter(): ViewPagerAdapter? {
